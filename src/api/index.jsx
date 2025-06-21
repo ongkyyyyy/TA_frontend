f524e8c46906997bd61d852925ba773e72279725
William Ongky Wijaya <william.ongkywow@gmail.com>
2025-06-21 23:23:34 +0700

    coba backendddd

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://127.0.0.1:5000";
+export const BASE_URL = "https://tabackend.up.railway.app";
 
 const useAxios = axios.create({
   baseURL: `${BASE_URL}`,
